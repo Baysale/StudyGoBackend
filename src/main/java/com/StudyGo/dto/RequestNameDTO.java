3f878262cb272ea54a669e3d6b94cdf65330cbca
harun <harun.dogan@stud.fh-campuswien.ac.at>
2024-01-22 00:15:09 +0100

    RequestNameDTO noArgsConstructor

--- src/main/java/com/StudyGo/dto/RequestNameDTO.java
+++ src/main/java/com/StudyGo/dto/RequestNameDTO.java
@@ -2,9 +2,11 @@ package com.StudyGo.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RequestNameDTO {
     private String name;
 }
